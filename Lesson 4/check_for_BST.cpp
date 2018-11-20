2f854851c207d8f2686bc8e743ff4998b534c96b
Marco Miani <44163115+IlMioFrizzantinoAmabile@users.noreply.github.com>
2018-11-20 18:07:46 +0100

    Update check_for_BST.cpp

--- Lesson 4/check_for_BST.cpp
+++ Lesson 4/check_for_BST.cpp
@@ -75,25 +75,6 @@ int main()
 }
 
 
-/*Please note that it's Function problem i.e.
-you need to write your solution in the form of Function(s) only.
-Driver Code to call/invoke your function is mentioned above.*/
-
-/* A binary tree node has data, pointer to left child
-   and a pointer to right child  
-struct Node {
-    int data;
-    Node* left, * right;
-}; */
-/* Should return true if tree represented by root is BST. 
-   For example, return  value should be 1 for following tree.
-         20
-     /      \ 
-   10       30
-  and return value should be 0 for following tree.
-         10
-     /      \ 
-   20       30 */
 
 const int MIN = 0;
 const int MAX = 1001;
