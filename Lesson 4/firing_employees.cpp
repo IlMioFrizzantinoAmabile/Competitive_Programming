1f7cdd6ea35e2163ea845babef8918c04c9e7af4
Marco Miani <44163115+IlMioFrizzantinoAmabile@users.noreply.github.com>
2018-11-20 18:04:56 +0100

    Update firing_employees.cpp

--- Lesson 4/firing_employees.cpp
+++ Lesson 4/firing_employees.cpp
@@ -1,3 +1,19 @@
+/* Mr. Alfred is the founder of FooLand Constructions.
+ * He always maintains a ‘Black list’ of potential employees which can be fired at any moment without any prior notice.
+ * This company has N employees (including Mr. Alfred), and each employee is assigned a rank (1 <= rank <= N) at the time of joining the company. Any person can have any rank from 1 to N. No two persons have the same rank. 
+ * The company has a hierarchically organized management. Each employee has one immediate senior but can have any number of seniors.
+ * If a person A is the senior of B and B is senior of C, this implies that A is also a senior of C. Note that Mr. Alfred has no senior to him.
+ * Mr. Alfred has a strange and unfair way of evaluating an employee’s performance. Those employees the sum of whose rank and the number of his/her seniors is a prime number is put up on the ‘Black list’.
+ * The task is to find out the number of ‘Black listed’ employees.
+ * Note: The list won’t contain Mr. Alfred’s name as he is the founder of the company and the list is managed by him!
+ * Input:
+ * The first line of input consists of a single integer T denoting the number of test cases. Then T test cases follow. Each test case consists of two lines. The first line contains a single integer N as described in the problem statement.
+ * The next line contains N integers. The ith integer (1 <= i <= N) represents the rank of the immediate senior of the ith employee (i.e. the employee with rank = i). If the ith integer is 0, it represents that employee with rank = i is Mr. Alfred as he has no superior.
+ * Output:
+ * For each test case, print in a new line the number of ‘Black listed’ employees.
+ * SOURCE: https://practice.geeksforgeeks.org/problems/firing-employees/0
+ */
+
 #include<bits/stdc++.h>
 
 using namespace std;
