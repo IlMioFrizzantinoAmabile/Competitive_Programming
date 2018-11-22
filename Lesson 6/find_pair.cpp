ae388a42ef1280c3e80f5927044ab20f050a67c5
Marco Miani <44163115+IlMioFrizzantinoAmabile@users.noreply.github.com>
2018-11-22 12:46:47 +0100

    Update find_pair.cpp

--- Lesson 6/find_pair.cpp
+++ Lesson 6/find_pair.cpp
@@ -1,3 +1,17 @@
+/* You've got another problem dealing with arrays. Let's consider an arbitrary sequence containing n (not necessarily different)
+ * integers a1, a2, ..., an. We are interested in all possible pairs of numbers (ai, aj), (1 ≤ i, j ≤ n). In other words, let's
+ * consider all n2 pairs of numbers, picked from the given array.
+ * Let's sort all resulting pairs lexicographically by non-decreasing. Let us remind you that pair (p1, q1) is lexicographically
+ * less than pair (p2, q2) only if either p1 < p2, or p1 = p2 and q1 < q2.
+ * The task is to find the k-th pair in the ordered list of all possible pairs of the array you've been given.
+ * Input:
+ * The first line contains two integers n and k. The second line contains the array containing n integers a1, a2, ..., an.
+ * The numbers in the array can coincide. All numbers are separated with spaces.
+ * Output:
+ * In the single line print two numbers — the sought k-th pair.
+ * SOURCE: http://codeforces.com/problemset/problem/160/C?locale=en
+ */
+
 #include<bits/stdc++.h>
 
 using namespace std;
