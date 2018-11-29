faba19e5af978be2a41ac36e7474d5537b487507
Marco Miani <44163115+IlMioFrizzantinoAmabile@users.noreply.github.com>
2018-11-29 18:40:09 +0100

    Update x_total_shapes.cpp

--- Lesson 12/x_total_shapes.cpp
+++ Lesson 12/x_total_shapes.cpp
@@ -1,3 +1,30 @@
+/* Given N * M string array of O's and X's return the number of 'X' total shapes.
+ * 'X' shape consists of one or more adjacent X's (diagonals not included).
+ * Example (1):
+ * OOOXOOO
+ * OXXXXXO
+ * OXOOOXO
+ * answer is 1 , shapes are:
+ * (i)     X
+ *     X X X X
+ *     X     X
+ * Example (2):
+ * XXX
+ * OOO
+ * XXX
+ * answer is 2, shapes are:
+ * (i)  XXX
+ * (ii) XXX
+ * Input:
+ * The first line of input takes the number of test cases, T.
+ * Then T test cases follow. Each of the T test cases takes 2 input lines.
+ * The first line of each test case have two integers N and M.
+ * The second line of N space separated strings follow which indicate the elements in the array.
+ * Output:
+ * Print number of shapes.
+ * SOURCE: https://practice.geeksforgeeks.org/problems/x-total-shapes/0
+ */
+
 #include<bits/stdc++.h>
 
 using namespace std;
