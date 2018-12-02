6e9ba535a0c14945fd738427067ac8a8fa967b2b
Marco Miani <44163115+IlMioFrizzantinoAmabile@users.noreply.github.com>
2018-12-02 22:47:19 +0100

    Update wilbur_and_array.cpp

--- Lesson 16/wilbur_and_array.cpp
+++ Lesson 16/wilbur_and_array.cpp
@@ -1,3 +1,16 @@
+/* Wilbur the pig is tinkering with arrays again. He has the array a1, a2, ..., an initially consisting of n zeros.
+ * At one step, he can choose any index i and either add 1 to all elements ai, ai + 1, ... , an or subtract 1 from
+ * all elements ai, ai + 1, ..., an. His goal is to end up with the array b1, b2, ..., bn.
+ * Of course, Wilbur wants to achieve this goal in the minimum number of steps and asks you to compute this value.
+ * Input:
+ * The first line of the input contains a single integer n, the length of the array ai. 
+ * Initially ai = 0 for every position i, so this array is not given in the input.
+ * The second line of the input contains n integers b1, b2, ..., bn.
+ * Output:
+ * Print the minimum number of steps that Wilbur needs to make in order to achieve ai = bi for all i.
+ * SOURCE: http://codeforces.com/problemset/problem/596/B
+ */
+
 #include<bits/stdc++.h>
 
 using namespace std;
