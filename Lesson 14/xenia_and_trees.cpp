dc30619289f7e9239dc185c74e7811c2c34d75b2
Marco Miani <44163115+IlMioFrizzantinoAmabile@users.noreply.github.com>
2018-12-02 16:26:34 +0100

    Update xenia_and_trees.cpp

--- Lesson 14/xenia_and_trees.cpp
+++ Lesson 14/xenia_and_trees.cpp
@@ -1,3 +1,22 @@
+/* Xenia the programmer has a tree consisting of n nodes. We will consider the tree nodes indexed from 1 to n.
+ * We will also consider the first node to be initially painted red, and the other nodes — to be painted blue.
+ * The distance between two tree nodes v and u is the number of edges in the shortest path between v and u.
+ * Xenia needs to learn how to quickly execute queries of two types:
+ * -(1) paint a specified blue node in red;
+ * -(2) calculate which red node is the closest to the given one and print the shortest distance to the closest red node.
+ * The task is to write a program which will execute the described queries.
+ * Input:
+ * The first line contains two integers n and m, the number of nodes in the tree and the number of queries.
+ * Next n - 1 lines contain the tree edges, the i-th line contains a pair of integers (ai, bi) an edge of the tree.
+ * Next m lines contain queries. 
+ * Each query is specified as a pair of integers ti, vi.
+ * If ti = 1, then as a reply to the query we need to paint a blue node vi in red. 
+ * If ti = 2, then we should reply to the query by printing the shortest distance from some red node to node vi.
+ * Output:
+ * For each second type query print the reply in a single line.
+ * SOURCE: http://codeforces.com/problemset/problem/342/E
+ */
+
 #include<bits/stdc++.h>
 
 using namespace std;
