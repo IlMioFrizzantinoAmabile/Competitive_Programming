18775a0eb2d771bd73151c1d8872c6e34a695c92
Marco Miani <44163115+IlMioFrizzantinoAmabile@users.noreply.github.com>
2018-12-02 22:45:35 +0100

    Update magic_numbers.cpp

--- Lesson 16/magic_numbers.cpp
+++ Lesson 16/magic_numbers.cpp
@@ -1,3 +1,13 @@
+/* A magic number is a number formed by concatenation of numbers 1, 14 and 144. We can use each of these numbers any number of times.
+ * Therefore 14144, 141414 and 1411 are magic numbers but 1444, 514 and 414 are not.
+ * You're given a number. Determine if it is a magic number or not.
+ * Input:
+ * The first line of input contains an integer n. This number doesn't contain leading zeros.
+ * Output:
+ * Print "YES" if n is a magic number or print "NO" if it's not.
+ * SOURCE: http://codeforces.com/problemset/problem/320/A
+ */
+
 #include<bits/stdc++.h>
 
 using namespace std;
