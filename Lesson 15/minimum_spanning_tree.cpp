a0ceee5ef9875842a9a37c6c665ca03093c2dc2e
Marco Miani <44163115+IlMioFrizzantinoAmabile@users.noreply.github.com>
2018-12-02 17:43:48 +0100

    Update minimum_spanning_tree.cpp

--- Lesson 15/minimum_spanning_tree.cpp
+++ Lesson 15/minimum_spanning_tree.cpp
@@ -1,3 +1,14 @@
+/* KRUSKAL ALGORITHM
+ * The task is to find the minimum spanning tree of the graph.
+ * Input:
+ * On the first line there will be two integers N, the number of nodes and M, the number of edges. (1 <= N <= 10000), (1 <= M <= 100000)
+ * M lines follow with three integers i j k on each line representing an edge between node i and j with weight k.
+ * The IDs of the nodes are between 1 and n inclusive. The weight of each edge will be <= 1000000.
+ * Output:
+ * Single number representing the total weight of the minimum spanning tree on this graph. There will be only one possible MST.
+ * SOURCE: https://www.spoj.com/problems/MST/
+ */
+
 #include<bits/stdc++.h>
 
 using namespace std;
