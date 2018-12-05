ac6e68488b62d0050a625c29c54534c40ee6e773
Marco Miani <44163115+IlMioFrizzantinoAmabile@users.noreply.github.com>
2018-12-05 10:37:56 +0100

    Update minimum_number_of_jumps.cpp

--- Lesson 19/minimum_number_of_jumps.cpp
+++ Lesson 19/minimum_number_of_jumps.cpp
@@ -1,3 +1,15 @@
+/* Given an array of integers where each element represents the max number of steps that can be made forward from that element.
+ * Write a function to return the minimum number of jumps to reach the end of the array (starting from the first element). 
+ * If an element is 0, then cannot move through that element.
+ * Input: 
+ * The first line contains an integer T, depicting total number of test cases. 
+ * Then following T lines contains a number n denoting the size of the array.
+ * Next line contains the sequence of integers a1, a2, ..., an.
+ * Output:
+ * Each seperate line showing the minimum number of jumps. If answer is not possible print -1.
+ * SOURCE: https://practice.geeksforgeeks.org/problems/minimum-number-of-jumps/0
+ */
+
 #include<bits/stdc++.h>
 
 using namespace std;
