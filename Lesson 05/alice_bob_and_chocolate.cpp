5d5b6e1a93215e27dc617ebd77783abafc0b5fee
Marco Miani <44163115+IlMioFrizzantinoAmabile@users.noreply.github.com>
2019-02-09 12:42:14 +0100

    Update alice_bob_and_chocolate.cpp

--- Lesson 05/alice_bob_and_chocolate.cpp
+++ Lesson 05/alice_bob_and_chocolate.cpp
@@ -1,4 +1,5 @@
-/* Alice and Bob like games. And now they are ready to start a new game. They have placed n chocolate bars in a line.
+/* PROBLEM
+ * Alice and Bob like games. And now they are ready to start a new game. They have placed n chocolate bars in a line.
  * Alice starts to eat chocolate bars one by one from left to right, and Bob — from right to left.
  * For each chocololate bar the time, needed for the player to consume it, is known (Alice and Bob eat them with equal speed).
  * When the player consumes a chocolate bar, he immediately starts with another.
@@ -6,10 +7,16 @@
  * If both players start to eat the same bar simultaneously, Bob leaves it to Alice as a true gentleman.
  * How many bars each of the players will consume?
  * Input:
- * The first line contains one integer n (1 ≤ n ≤ 105) — the amount of bars on the table. The second line contains a sequence t1, t2, ..., tn (1 ≤ ti ≤ 1000), where ti is the time (in seconds) needed to consume the i-th bar (in the order from left to right).
+ * The first line contains one integer n, the amount of bars on the table.
+ * The second line contains a sequence t1, t2, ..., tn, where ti is the time (in seconds) needed to consume the i-th bar (in the order from left to right).
  * Output:
  * Print two numbers a and b, where a is the amount of bars consumed by Alice, and b is the amount of bars consumed by Bob.
  * SOURCE: http://codeforces.com/problemset/problem/6/C?locale=en
+ *
+ * SOLUTION
+ * L'algoritmo mentre legge i tempi (bars[]) necessari a mangiare ciascuna barretta, ne calcola la somma.
+ * Dopodiché "assegna" barrette ad Alice finché non raggiunge la metà, e infine aggiusta il caso Bob fa il true gentleman.
+ * COMPLEXITY: O(n)
  */
 
 #include<bits/stdc++.h>
