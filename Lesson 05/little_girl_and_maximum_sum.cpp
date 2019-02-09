d9f3dd08493b5ad148360f38d76e8937e9f5a87c
Marco Miani <44163115+IlMioFrizzantinoAmabile@users.noreply.github.com>
2019-02-09 12:55:59 +0100

    Update little_girl_and_maximum_sum.cpp

--- Lesson 05/little_girl_and_maximum_sum.cpp
+++ Lesson 05/little_girl_and_maximum_sum.cpp
@@ -1,4 +1,5 @@
-/* The little girl loves the problems on array queries very much.
+/* PROBLEM
+ * The little girl loves the problems on array queries very much.
  * One day she came across a rather well-known problem: you've got an array of n elements (the elements of the array are indexed starting from 1);
  * also, there are q queries, each one is defined by a pair of integers li, ri (1 ≤ li ≤ ri ≤ n).
  * You need to find for each query the sum of elements of the array with indexes from li to ri, inclusive.
@@ -6,12 +7,18 @@
  * She decided to reorder the array elements before replying to the queries in a way that makes the sum of query replies maximum possible.
  * Your task is to find the value of this maximum sum.
  * Input:
- * The first line contains two space-separated integers n (1 ≤ n ≤ 2·105) and q (1 ≤ q ≤ 2·105) — the number of elements in the array and the number of queries, correspondingly.
- * The next line contains n space-separated integers ai (1 ≤ ai ≤ 2·105) — the array elements.
+ * The first line contains two space-separated integers n and q,the number of elements in the array and the number of queries, correspondingly.
+ * The next line contains n space-separated integers ai, the array elements.
  * Each of the following q lines contains two space-separated integers li and ri (1 ≤ li ≤ ri ≤ n) — the i-th query.
  * Output:
  * In a single line print a single integer — the maximum sum of query replies after the array elements are reordered.
  * SOURCE: http://codeforces.com/problemset/problem/276/C?locale=en
+ *
+ * SOLUTION
+ * L'algoritmo memorizza le query in un array con +1 e -1 rispettivamente nei due estremi.
+ * Calcola la prefix sum di questo array per ottenere in quante query compare ciascuna posizione, in tempo lineare.
+ * Sorta posizioni (per numero di occorenze) e array (per valore). Calcola il max con una semplice somma.
+ * COMPLEXITY: O(n*log(n)+q)
  */
 
 #include<bits/stdc++.h>
