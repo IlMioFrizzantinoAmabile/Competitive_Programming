bcaf6757d803790cc965ed9ae29b992986a308e3
Marco Miani <44163115+IlMioFrizzantinoAmabile@users.noreply.github.com>
2019-02-09 11:17:05 +0100

    Update sliding_window_maximum.cpp

--- Lesson 01/sliding_window_maximum.cpp
+++ Lesson 01/sliding_window_maximum.cpp
@@ -1,4 +1,5 @@
-/* Given an array A and an integer K. Find the maximum for each and every contiguous subarray of size K.
+/* PROBLEM
+ * Given an array A and an integer K. Find the maximum for each and every contiguous subarray of size K.
  * Input:
  * The first line of input contains an integer T denoting the number of test cases.
  * The first line of each test case contains a single integer N denoting the size of array and the size of subarray K.
@@ -6,6 +7,16 @@
  * Output:
  * Print the maximum for every subarray of size k.
  * SOURCE: https://practice.geeksforgeeks.org/problems/maximum-of-all-subarrays-of-size-k/0
+ *
+ * SOLUTION
+ * L'algoritmo utilizza una deque di coppie.
+ * Il primo elemento di ogni coppia è il valore di un elemento dell'array, il secondo è la sua posizione.
+ * L'algoritmo scorre l'array. In ogni momento i primi valori delle coppie nella deque sono in ordine decrescente (*).
+ * Ad ogni passo legge un nuovo valore (x) dell'array.
+ * Elimina dalla deque tutti gli elementi che non sono più nel range di interesse (i-k,i] e tutti gli elementi più piccoli di x.
+ * Aggiunge l'elemento x in fondo dalla deque. Questo garantisce il mantenimento della proprietà (*).
+ * Il massimo nel range è quindi dato dall'elemento in testa alla deque.
+ * COMPLEXITY: O(n)
  */
 
 #include<bits/stdc++.h>
