7b2944db2fd6a82954d06674136a841ea0b33612
Marco Miani <44163115+IlMioFrizzantinoAmabile@users.noreply.github.com>
2019-02-09 11:26:05 +0100

    Update trapping_rain_water.cpp

--- Lesson 01/trapping_rain_water.cpp
+++ Lesson 01/trapping_rain_water.cpp
@@ -1,4 +1,5 @@
-/* Given an array A of N non-negative integers representing height of blocks at index i as Ai, where the width of each block is 1.
+/* PROBLEM
+ * Given an array A of N non-negative integers representing height of blocks at index i as Ai, where the width of each block is 1.
  * Compute how much water can be trapped in between blocks after raining.
  * Input:
  * The first line of input contains an integer T denoting the number of test cases.
@@ -6,6 +7,11 @@
  * Output:
  * Print the total unit of water trapped in between the blocks.
  * SOURCE: https://practice.geeksforgeeks.org/problems/trapping-rain-water/0
+ *
+ * SOLUTION
+ * L'algoritmo tiene in memoria due puntatori (sx,dx) che delimitano un sotto-intervallo dell'array, all'inizio tutto.
+ * Ad ogni passo questo intervallo viene stretto, dal lato con altezza minore, aggiungendo acqua finché trova un muro più alto.
+ * COMPLEXITY: O(n)
  */
 
 #include<bits/stdc++.h>
