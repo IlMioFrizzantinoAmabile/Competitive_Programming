25caffbd3e141449930c8311a2511e77ed68a17f
Marco Miani <44163115+IlMioFrizzantinoAmabile@users.noreply.github.com>
2019-02-10 18:45:49 +0100

    Update longest_common_subsequence.cpp

--- Lesson 18/longest_common_subsequence.cpp
+++ Lesson 18/longest_common_subsequence.cpp
@@ -1,4 +1,5 @@
-/* Given two sequences, find the length of longest subsequence present in both of them. Both the strings are of uppercase.
+/* PROBLEM
+ * Given two sequences, find the length of longest subsequence present in both of them. Both the strings are of uppercase.
  * Input:
  * First line of the input contains no of test cases T, the T test cases follow.
  * Each test case consist of 2 space separated integers A and B denoting the size of string str1 and str2 respectively
@@ -6,6 +7,10 @@
  * Output:
  * For each test case print the length of longest common subsequence of the two strings .
  * SOURCE: https://practice.geeksforgeeks.org/problems/longest-common-subsequence/0
+ *
+ * SOLUTION
+ * L'algoritmo utilizza la programmazione dinamica risolvendo i sottoproblemi che hanno i prefissi delle stringhe.
+ * COMPLEXITY: O(A*B)
  */
 
 #include<bits/stdc++.h>
@@ -16,9 +21,9 @@ int tab[101][101];
 
 int LCS (string s, string t) {
 	for (int i=1;i<=s.length();i++) for (int j=1;j<=t.length();j++) {
-		if (i==0 or j==0) 			tab[i][j] = 0;
+		if (i==0 or j==0) 		tab[i][j] = 0;
 		else if (s[i-1]==t[j-1])	tab[i][j] = 1+tab[i-1][j-1];
-		else 						tab[i][j] = max(tab[i][j-1],tab[i-1][j]);
+		else 				tab[i][j] = max(tab[i][j-1],tab[i-1][j]);
 	}
 	return tab[s.length()][t.length()];
 }
