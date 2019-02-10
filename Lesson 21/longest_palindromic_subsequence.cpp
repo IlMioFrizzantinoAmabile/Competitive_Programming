42f8888da5371c7f614b4d77b07e5ee25ffa4347
Marco Miani <44163115+IlMioFrizzantinoAmabile@users.noreply.github.com>
2019-02-10 19:56:35 +0100

    Update longest_palindromic_subsequence.cpp

--- Lesson 21/longest_palindromic_subsequence.cpp
+++ Lesson 21/longest_palindromic_subsequence.cpp
@@ -1,10 +1,16 @@
-/* Given a String, find the longest palindromic subsequence.
+/* PROBLEM
+ * Given a String, find the longest palindromic subsequence.
  * Input:
  * The first line of input contains an integer T, denoting no of test cases.
  * The only line of each test case consists of a string S (only lowercase)
  * Output:
  * Print the Maximum length possible for palindromic subsequence.
  * SOURCE: https://practice.geeksforgeeks.org/problems/longest-palindromic-subsequence/0
+ *
+ * SOLUTION
+ * L'algoritmo utilizza la programmazione dinamica risolvendo i sottoproblemi sulle sottoparole, cioè i caratteri dall'i-esimio al
+ * j-esimo per ogni coppia i,j.
+ * COMPLEXITY: O(length^2)
  */
 
 #include<bits/stdc++.h>
@@ -20,9 +26,9 @@ int longestPalindromicSequence(string s) {
 	    for (int i=0;i<=len-substrLeng;i++) {
     		int j=i+substrLeng;
     		if (substrLeng==0) 		tab[i][j] = 0;
-    		else if (substrLeng==1) tab[i][j] = 1;
-    		else if (s[i]==s[j-1])	tab[i][j] = 2 + tab[i+1][j-1];
-    		else 					tab[i][j] = max(tab[i+1][j], tab[i][j-1]);
+    		else if (substrLeng==1)		tab[i][j] = 1;
+    		else if (s[i]==s[j-1])		tab[i][j] = 2 + tab[i+1][j-1];
+    		else 				tab[i][j] = max(tab[i+1][j], tab[i][j-1]);
 	    }
 	return tab[0][len];
 }
