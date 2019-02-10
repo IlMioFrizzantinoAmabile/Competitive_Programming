0de7e1e918a0b2109a52b48809c9dce5cc4dfe38
Marco Miani <44163115+IlMioFrizzantinoAmabile@users.noreply.github.com>
2019-02-10 18:11:40 +0100

    Update wilbur_and_array.cpp

--- Lesson 16/wilbur_and_array.cpp
+++ Lesson 16/wilbur_and_array.cpp
@@ -1,4 +1,5 @@
-/* Wilbur the pig is tinkering with arrays again. He has the array a1, a2, ..., an initially consisting of n zeros.
+/* PROBLEM
+ * Wilbur the pig is tinkering with arrays again. He has the array a1, a2, ..., an initially consisting of n zeros.
  * At one step, he can choose any index i and either add 1 to all elements ai, ai + 1, ... , an or subtract 1 from
  * all elements ai, ai + 1, ..., an. His goal is to end up with the array b1, b2, ..., bn.
  * Of course, Wilbur wants to achieve this goal in the minimum number of steps and asks you to compute this value.
@@ -9,6 +10,10 @@
  * Output:
  * Print the minimum number of steps that Wilbur needs to make in order to achieve ai = bi for all i.
  * SOURCE: http://codeforces.com/problemset/problem/596/B
+ *
+ * SOLUTION
+ * La soluzione è il valore assoluto del primo elemento più la somma dei valori assoluti delle differenze degli elementi consecutivi.
+ * COMPLEXITY: O(n)
  */
 
 #include<bits/stdc++.h>
